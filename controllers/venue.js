208e51d697f5d614d832d6275ef57bb4f0958ccd
narendramanchala <mvnarendrareddy@gmail.com>
2017-10-20 12:10:30 +0530

    basic form validation is done for email and phone

--- controllers/venue.js
+++ controllers/venue.js
@@ -9,45 +9,59 @@ var storage = multer.diskStorage({
         cb(null, file.fieldname + '-' + Date.now()+'.jpg')
     }
 });
-console.log(Date.now());
+// console.log(Date.now());
 var upload = multer({ storage: storage }).single('file');
 
 exports.createVenueForm = (req, res) => {
-//
-    // const imagePath = req.files.upload.path;
-    // const imageName = req.files.upload.name;
-    upload(req, res, function (err) {
-        if (err) {
-            // An error occurred when uploading
-            return res.json({
-                err:err
+    console.log(req.body);
 
-            })
-        }
-        Venue.create({
-            name: req.body.name,
-            address: req.body.address,
-            email: req.body.email,
-            phone: req.body.phone,
-            description: req.body.description,
-            sports: [{name:"cricket"},{name:"football"}],
-            // photos:[{name:req.files.upload.name, path:req.files.upload.path}]
-        }, function(err, venue){
-            if(err){console.log(err)
-            return res.json({err:err})
-            }else{
-                console.log(venue);
 
-                res.json(venue);
+    // setInterval(function(){
+    //     console.log(req.body)
+    // }, 100)
+
+
+        upload(req, res, function (err) {
+            console.log("inside upload");
+            if (err) {
+                // An error occurred when uploading
+                return res.json({
+                    err: err
+
+                })
             }
-        })
+            req.checkBody("email", "Enter a valid email address.").isEmail();
+            req.checkBody("phone","enter a valid mobile number").isMobilePhone("en-IN");
+            var errors = req.validationErrors();
+            if (errors) {
+                res.send(errors);
+                return;
+            } else {
+                // normal processing here
+            Venue.create({
+                name: req.body.name,
+                address: req.body.address,
+                email: req.body.email,
+                phone: req.body.phone,
+                description: req.body.description,
+                sports: [{name: "cricket"}, {name: "football"}],
+                // photos:[{name:req.files.upload.name, path:req.files.upload.path}]
+            }, function (err, venue) {
+                if (err) {
+                    console.log(err)
+                    return res.json({err: err})
+                } else {
+                    console.log(venue);
+                    res.json(venue);
+                }
+            })
 
-        // res.json({
-        //     success: true,
-        //     message: "Image Uploaded"
-        // })
+            // res.json({
+            //     success: true,
+            //     message: "Image Uploaded"
+            // })
+        }
     });
-
 };
 
 //
